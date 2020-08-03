97987b63f78089c5b15e2845384ef93f23bbb640
Andrew Leonard <andrew_m_leonard@uk.ibm.com>
2020-08-03 21:25:08 +0100

    Trigger remote

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -175,13 +175,7 @@ class Build {
 						def remote = true
                  				if (remote) {
 							def handle = triggerRemoteJob(remoteJenkinsName: 'hyc-runtimes', job: jobName,
-                                                                                parameters: [
-												context.string(name: 'SDK_RESOURCE', value: "customized"),
-                                                                                                context.string(name: 'CUSTOMIZED_SDK_URL', value: "http://adoptjenkinsci1.fyre.ibm.com:8080/job/build-scripts/job/jobs/job/jdk11u/job/jdk11u-linux-x64-openj9/lastSuccessfulBuild/artifact/workspace/target/OpenJDK11U-jdk_x64_linux_openj9_2020-08-02-23-30.tar.gz"),
-                                                                                                context.string(name: 'RELEASE_TAG', value: "${buildConfig.SCM_REF}"),
-                                                                                                context.string(name: 'JDK_REPO', value: jdkRepo),
-                                                                                                context.string(name: 'JDK_BRANCH', value: jdkBranch)]
-											)
+                                                                                parameters: "SDK_RESOURCE=customized, CUSTOMIZED_SDK_URL=http://adoptjenkinsci1.fyre.ibm.com:8080/job/build-scripts/job/jobs/job/jdk11u/job/jdk11u-linux-x64-openj9/lastSuccessfulBuild/artifact/workspace/target/OpenJDK11U-jdk_x64_linux_openj9_2020-08-02-23-30.tar.gz, RELEASE_TAG=${buildConfig.SCM_REF}, JDK_REPO=${jdkRepo}, JDK_BRANCH=${jdkBranch}")
 							def status = handle.getBuildStatus()
 							def buildUrl = handle.getBuildUrl()
 							echo buildUrl.toString() + " finished with " + status.toString()
